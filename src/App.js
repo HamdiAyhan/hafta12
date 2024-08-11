62d8bd1629d8d6a1b407203961f500188f683ce7
HamdiAyhan <hamdiminer1@gmail.com>
2024-08-11 16:19:37 +0300

    bootstrap paketi projeye eklendi

--- src/App.js
+++ src/App.js
@@ -2,9 +2,37 @@
 
 function App() {
   return (
-    <div className="App">
-
+    <section className="container mt-5">
+      <div className="row" >
+        <div className="col" >
+        <ol class="list-group list-group-numbered">
+  <li class="list-group-item d-flex justify-content-between align-items-start">
+    <div class="ms-2 me-auto">
+      <div class="fw-bold">Subheading</div>
+      Content for list item
     </div>
+    <span class="badge text-bg-primary rounded-pill">14</span>
+  </li>
+  <li class="list-group-item d-flex justify-content-between align-items-start">
+    <div class="ms-2 me-auto">
+      <div class="fw-bold">Subheading</div>
+      Content for list item
+    </div>
+    <span class="badge text-bg-primary rounded-pill">14</span>
+  </li>
+  <li class="list-group-item d-flex justify-content-between align-items-start">
+    <div class="ms-2 me-auto">
+      <div class="fw-bold">Subheading</div>
+      Content for list item
+    </div>
+    <span class="badge text-bg-primary rounded-pill">14</span>
+  </li>
+</ol>
+
+        </div>
+      </div>
+
+    </section>
   );
 }
 
