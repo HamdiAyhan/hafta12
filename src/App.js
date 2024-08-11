1580e57ddf215b87f343f9df1ea22562d348aad9
HamdiAyhan <hamdiminer1@gmail.com>
2024-08-11 20:27:50 +0300

    bitti

--- src/App.js
+++ src/App.js
@@ -1,50 +1,52 @@
-import { Alert } from "react-bootstrap";
-import Badge from 'react-bootstrap/Badge';
-import ListGroup from 'react-bootstrap/ListGroup';
-import { useState ,useEffect} from "react";
-import CallMadeIcon from '@mui/icons-material/CallMade';
-import HaberListe from "./HaberListe";
+import {Alert} from 'react-bootstrap';
+import {useState, useEffect} from 'react';
+import HaberListe from './HaberListe';
 
+function App () {
+  const [sorgu, setSorgu] = useState ('javascript');
+  const url = `https://hn.algolia.com/api/v1/search?query=${sorgu}`;
+  const [veri, setveri] = useState ({hits: []});
+  const [loading, setLoading] = useState (false);
+  const [inpuveri, setinpuVeri] = useState ('');
 
+  useEffect (
+    () => {
+      setLoading (true);
+      const veriCek = async () => {
+        const sonuc = await fetch (url);
+        const jsSonuc = await sonuc.json ();
+        setveri (jsSonuc);
+        setLoading (false);
+      };
+      veriCek ();
+    },
+    [sorgu]
+  );
 
-
-function App() {
-   const [sorgu, setSorgu] = useState("javascript")
-  const url=`https://hn.algolia.com/api/v1/search?query=${sorgu}`
-  const [veri, setveri] = useState({hits : []})
- const [loading, setLoading] = useState(false)
-  const [inpuveri, setinpuVeri] = useState("")
-
-
-  useEffect(() => {
-    setLoading(true)
-   const veriCek = async ()=>{
-    const sonuc = await fetch (url)
-    const jsSonuc = await sonuc.json()
-    setveri(jsSonuc)
-    setLoading(false)
-   }
-   veriCek()
-  },[sorgu])
-
-  function aramaBaslat(){
-setSorgu(inpuveri)
+  function aramaBaslat () {
+    setSorgu (inpuveri);
   }
 
-
-  console.log(veri.hits)
+  console.log (veri.hits);
   return (
     <section className="container mt-5">
-      <div className="row" >
-        <div className="col" >
-          <div className="d-flex justify-content-between align-items-center" >
-              <h1>{sorgu} Haberler</h1>
-              <input onChange={(olay)=>setinpuVeri(olay.target.value)} className="form-control" type="text " placeholder="Ne Aramıştınız ?"></input>
-              <buton onClick={aramaBaslat} className="btn btn-primary btn-sm" >Ara</buton>
+      <div className="row">
+        <div className="col">
+          <div className="d-flex justify-content-between align-items-center">
+            <h1>{sorgu} Haberler</h1>
+            <input
+              onChange={olay => setinpuVeri (olay.target.value)}
+              className="form-control"
+              type="text "
+              placeholder="Ne Aramıştınız ?"
+            />
+            <buton onClick={aramaBaslat} className="btn btn-primary btn-sm">
+              Ara
+            </buton>
           </div>
-          {loading === true ? <p>Loading.....</p> : <HaberListe veri={veri} /> }
- 
-<Alert variant="success" >selam N'ber</Alert>
+          {loading === true ? <p>Loading.....</p> : <HaberListe veri={veri} />}
+
+          <Alert variant="success">selam N'ber</Alert>
         </div>
       </div>
 
